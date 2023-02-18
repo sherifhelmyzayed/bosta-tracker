7ea95095b2f885e4d82ca562735a508f48611709
sherif zayed <sherifhelmyzayed@gmail.com>
2023-02-18 23:58:47 +0200

    feat: added blank custom theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,67 @@
 import './App.css';
-import { Grid } from '@mui/material';
+import { createTheme, Grid, ThemeProvider } from '@mui/material';
 
 import AddressInfo from './components/AddressInfo';
 import Header from './components/Header';
 import PackageInfo from './components/PackageInfo';
 import PackageHistory from './components/PackageHistory';
+import { green, orange, purple } from '@mui/material/colors';
+
+// declare module '@mui/material/styles' {
+//   interface Theme {
+//     status: {
+//       danger: string;
+//     };
+//   }
+//   // allow configuration using `createTheme`
+//   interface ThemeOptions {
+//     status?: {
+//       danger?: string;
+//     };
+//   }
+// }
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: purple[500],
+    },
+    secondary: {
+      main: green[500],
+    },
+  },
+  typography:{
+    h1: {
+      fontSize: 2
+    },
+
+  }
+  // ,
+  // status: {
+  //   danger: orange[500],
+  // },
+});
 
 function App() {
 
   return (
-    <div dir="rtl">
-      <Header />
-      <PackageInfo />
-      <Grid container>
-        <Grid item md={8}>
-          <div>package history</div>
-          <PackageHistory />
+    <ThemeProvider theme={theme}>
+      <div dir="rtl">
+        <Header />
+        <PackageInfo />
+        <Grid container>
+          <Grid item md={8}>
+            <div>package history</div>
+            <PackageHistory />
+          </Grid>
+          <Grid item md={2}>
+            <div>Adress Info</div>
+            <AddressInfo />
+          </Grid>
         </Grid>
-        <Grid item md={2}>
-          <div>Adress Info</div>
-          <AddressInfo />
-        </Grid>
-      </Grid>
-    </div>
+      </div>
+    </ThemeProvider>
+
   )
 }
 
